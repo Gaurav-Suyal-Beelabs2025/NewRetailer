709c47df939bea99457a976b7947d8c56dcc9c54
unknown <ritikr@beelabs.technology>
2023-04-10 19:35:33 +0530

    [FEAT] :  Added UIs

---
+++ app/src/main/java/com/suyal/newretailer/Order_details_Fragment.kt
@@ -0,0 +1,18 @@
+package com.suyal.newretailer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Order_details_Fragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order_details_, container, false)
+    }
+
+}
